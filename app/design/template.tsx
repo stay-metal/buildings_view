b3c04522f1ea80f69c7b78f5a70e9cd0bc838077
vadrums <vadlambrianov@gmail.com>
2024-07-24 16:19:27 +0300

    feat: New structure implemented

---
+++ app/design/template.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { Header } from "@/components/Header";
+import FullSreen from "@/components/Buttons/FullScreen";
+import Logo from "@/components/ui/Logo";
+import Share from "@/components/Buttons/Share";
+import Footer from "@/components/Footer";
+import { Thumbnails } from "@/components/Slider/Thumbnails";
+
+export default function Template({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,7 @@
 "use client";
-import type { Metadata } from "next";
+
 import { Providers } from "./providers";
-import { Box } from "@mui/material";
-import { Header } from "@/components/Header";
-import FullSreen from "@/components/Buttons/FullScreen";
-import Logo from "@/components/ui/Logo";
-import Share from "@/components/Buttons/Share";
-import Footer from "@/components/Footer";
 import "./globals.css";
-import { Thumbnails } from "@/components/Slider/Thumbnails";
 
 export default function RootLayout({
   children,
@@ -19,18 +12,7 @@ export default function RootLayout({
     <html lang="en">
       <meta name="viewport" content="initial-scale=1, width=device-width" />
       <body>
-        <Providers>
-          <Header>
-            <FullSreen />
-            <Logo />
-            <Share />
-          </Header>
-
-          {children}
-          <Footer>
-            <Thumbnails />
-          </Footer>
-        </Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
